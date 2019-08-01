cd5a9b056c2162770e496a081a86781edea5e51e
Khoa Do <happyboy0412@gmail.com>
2019-08-01 13:37:08 +0700

    move player and bullet to app

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::image::{LoadTexture, InitFlag};
 use sdl2::rect::Rect;
-use sdl2::render::{Texture, WindowCanvas};
+use sdl2::render::{Texture, TextureCreator, WindowCanvas};
+use sdl2::video::WindowContext;
 use std::env;
 use std::path::Path;
 use std::time::Duration;
@@ -32,39 +33,57 @@ struct Entity<'a> {
     texture: Texture<'a>,
 }
 
-struct App {
+struct App<'a> {
     up: u8,
     down: u8,
     left: u8,
     right: u8,
     fire: u8,
+    player: Entity<'a>,
+    bullet: Entity<'a>,
 }
 
-fn init() -> App {
-    App {
+fn init<'a>(texture_creator: &'a TextureCreator<WindowContext>, player_img: &Path, bullet_img: &Path) -> Result<App<'a>, String> {
+    Ok(App {
         up: 0,
         down: 0,
         left: 0,
         right: 0,
         fire: 0,
-    }
+        player: Entity {
+            x: 100,
+            y: 100,
+            dx: 4,
+            dy: 4,
+            health: 0,
+            texture: texture_creator.load_texture(player_img)?,
+        },
+        bullet: Entity {
+            x: 0,
+            y: 0,
+            dx: 16,
+            dy: 0,
+            health: 0,
+            texture: texture_creator.load_texture(bullet_img)?,
+        },
+    })
 }
 
-fn render(canvas: &mut WindowCanvas, app: &App, player: &Entity, bullet: &Entity) -> Result<(), String> {
+fn render(canvas: &mut WindowCanvas, app: &App) -> Result<(), String> {
     let player_dest = Rect::new(
-        player.x,
-        player.y,
-        player.texture.query().width,
-        player.texture.query().height);
-    canvas.copy(&player.texture, None, Some(player_dest))?;
+        app.player.x,
+        app.player.y,
+        app.player.texture.query().width,
+        app.player.texture.query().height);
+    canvas.copy(&app.player.texture, None, Some(player_dest))?;
 
-    if bullet.health == 1 {
+    if app.bullet.health == 1 {
         let bullet_dest = Rect::new(
-            bullet.x,
-            bullet.y,
-            bullet.texture.query().width,
-            bullet.texture.query().height);
-        canvas.copy(&bullet.texture, None, Some(bullet_dest))?;
+            app.bullet.x,
+            app.bullet.y,
+            app.bullet.texture.query().width,
+            app.bullet.texture.query().height);
+        canvas.copy(&app.bullet.texture, None, Some(bullet_dest))?;
     }
 
     canvas.present();
@@ -128,24 +147,8 @@ fn run (player_img: &Path, bullet_img: &Path) -> Result<(), String> {
     let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
 
-    let mut app = init();
+    let mut app = init(&texture_creator, player_img, bullet_img)?;
 
-    let mut player = Entity {
-        x: 100,
-        y: 100,
-        dx: 4,
-        dy: 4,
-        health: 0,
-        texture: texture_creator.load_texture(player_img)?,
-    };
-    let mut bullet = Entity {
-        x: 0,
-        y: 0,
-        dx: 16,
-        dy: 0,
-        health: 0,
-        texture: texture_creator.load_texture(bullet_img)?,
-    };
     let mut event_pump = sdl_context.event_pump()?;
     canvas.set_draw_color(Color::RGB(98, 128, 255));
 
@@ -169,31 +172,31 @@ fn run (player_img: &Path, bullet_img: &Path) -> Result<(), String> {
         }
 
         if app.up == 1 {
-            player.y = player.y - player.dy;
+            app.player.y = app.player.y - app.player.dy;
         }
         if app.down == 1 {
-            player.y = player.y + player.dy;
+            app.player.y = app.player.y + app.player.dy;
         }
         if app.left == 1 {
-            player.x = player.x - player.dx;
+            app.player.x = app.player.x - app.player.dx;
         }
         if app.right == 1 {
-            player.x = player.x + player.dx;
+            app.player.x = app.player.x + app.player.dx;
         }
 
-        if app.fire == 1 && bullet.health == 0 {
-            bullet.health = 1;
-            bullet.x = player.x;
-            bullet.y = player.y + 10;
+        if app.fire == 1 && app.bullet.health == 0 {
+            app.bullet.health = 1;
+            app.bullet.x = app.player.x;
+            app.bullet.y = app.player.y + 10;
         }
-        if bullet.health == 1 {
-            bullet.x = bullet.x + bullet.dx;
+        if app.bullet.health == 1 {
+            app.bullet.x = app.bullet.x + app.bullet.dx;
         }
-        if bullet.x > 800 {
-            bullet.health = 0;
+        if app.bullet.x > 800 {
+            app.bullet.health = 0;
         }
 
-        render(&mut canvas, &app, &player, &bullet)?;
+        render(&mut canvas, &app)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
